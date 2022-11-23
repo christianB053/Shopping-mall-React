d250f2e122e4790065ab50aa03d64ed892e9cda4
hb_kim <binikim66@gmail.com>
2022-11-24 00:47:12 +0900

    product image 생성, 스타일 적용

--- my-app/src/components/Card/Card.jsx
+++ my-app/src/components/Card/Card.jsx
@@ -1,3 +1,28 @@
-export default function Card() {
-  return <div></div>;
+import ProductImage from "./ProductImage/ProductImage";
+import "./card.css";
+
+export default function Card({
+  productName,
+  price,
+  thumbnailImg,
+  discountRate,
+}) {
+  return (
+    <li>
+      <div className="product-img">
+        <ProductImage {...{ thumbnailImg, productName }} />
+      </div>
+      <strong>이름</strong>
+      <button></button>
+      <div>
+        <strong>할인가격</strong>
+        <p>
+          원래가격<span>원</span>
+        </p>
+        <p>
+          할인율<span>원</span>
+        </p>
+      </div>
+    </li>
+  );
 }
--- my-app/src/pages/HomePage/HomePage.jsx
+++ my-app/src/pages/HomePage/HomePage.jsx
@@ -16,7 +16,7 @@ export default function HomePage() {
         <>
           <ul>
             {loadData.map((item) => (
-              <Card key={item.id} data={item} />
+              <Card key={item.id} {...item} />
             ))}
           </ul>
           <Cart />
